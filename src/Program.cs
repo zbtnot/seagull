d586a291798bc53a19b64b2554ad1bcc7b63912e
Giorgio Arcamone <giorgio@gioarc.me>
2024-02-21 11:52:44 -0500

    Add commandline parsing

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Seagull;
+﻿using CommandLine;
+using Seagull.Model.Option;
+
+namespace Seagull;
 
 internal static class Program
 {
     public static int Main(string[] args)
     {
-        Console.WriteLine("TODO!");
+        try
+        {
+            Parser.Default
+                .ParseArguments<BuildProject, GenerateProject>(args)
+                .WithParsed(RunOptions);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return 1;
+        }
+
         return 0;
+    }
+
+    static void RunOptions(object optionType)
+    {
+        switch (optionType)
+        {
+            case BuildProject option:
+                break;
+            case GenerateProject option:
+                break;
+        }
     }
 }
